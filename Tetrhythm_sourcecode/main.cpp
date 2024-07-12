2dd9e4aae8dfb4631363459352121db1d2e1ca63
YujinJeon888888 <117812553+YujinJeon888888@users.noreply.github.com>
2024-07-13 01:33:26 +0900

    feat: 프레임처리

--- Tetrhythm_sourcecode/main.cpp
+++ Tetrhythm_sourcecode/main.cpp
@@ -1,5 +1,7 @@
 #include "Print.h"
 #include <vector>
+#include <thread>
+#include <chrono>
 
 int main(int argc, char* argv[]) {
     Print print;
@@ -10,12 +12,20 @@ int main(int argc, char* argv[]) {
 
     // 애니메이션 출력
     std::vector<std::string> animPaths1 = { "frame1.png", "frame2.png" };
-    print.printAnimationPNG(animPaths1, 200, 200, 3, 240); // 240 프레임마다 애니메이션 출력
-    
+    print.printAnimationPNG(animPaths1, 200, 200, 3, 60); // 1초마다 애니메이션 출력
+    std::vector<std::string> animPaths2 = { "Anim1.png", "Anim2.png" };
+    print.printAnimationPNG(animPaths2, 200, 200, 4, 60); // 1초마다 애니메이션 출력
+
+    const int targetFPS = 60;
+    const std::chrono::milliseconds frameDuration(1000 / targetFPS);
+
     while (true) {
         print.handleEvents();
         print.updateAnimations(); // 애니메이션 업데이트
         print.render(); // 렌더링
+        
+        // 프레임 속도 제어
+        std::this_thread::sleep_for(frameDuration);
     }
 
     return 0;

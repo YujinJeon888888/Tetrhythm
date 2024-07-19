c06a84f8c15fea95bdfd337a9e85fda7d9db762a
YujinJeon888888 <117812553+YujinJeon888888@users.noreply.github.com>
2024-07-19 19:34:57 +0900

    feat: 로컬연동테스트

--- Tetrhythm_sourcecode/SQLTest.cpp
+++ Tetrhythm_sourcecode/SQLTest.cpp
@@ -1,9 +1,36 @@
 #include <mysql.h>
-
-#pragma comment(lib, "libmySQL.lib")
 #include <iostream>
+#pragma comment(lib, "libmySQL.lib")
 using namespace std;
 int main() {
-	cout << mysql_get_client_info() << endl;
+
+	MYSQL Conn;
+	MYSQL* ConnPtr = NULL;
+	MYSQL_RES* Result;
+	MYSQL_ROW Row;
+	int Stat;
+
+	mysql_init(&Conn);
+
+	ConnPtr = mysql_real_connect(&Conn, "localhost", "root", "8501", "test", 3306, (char*)NULL, 0);
+	if (ConnPtr == NULL) {
+		cout << mysql_error(&Conn) << endl;
+		return 1;
+	}
+
+	const char* Query = "select * from testtable";
+	Stat = mysql_query(ConnPtr, Query);
+	if (Stat != 0) {
+		cout << mysql_error(&Conn) << endl;
+		return 1;
+	}
+
+	Result = mysql_store_result(ConnPtr);
+	while ((Row = mysql_fetch_row(Result)) != NULL) {
+		cout << Row[0] << endl;
+	}
+	mysql_free_result(Result);
+	mysql_close(ConnPtr);
 	return 0;
+
 }

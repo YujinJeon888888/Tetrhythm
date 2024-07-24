0cfb6967f3c92f037f9edd0c0ce60bd4df712376
YujinJeon888888 <117812553+YujinJeon888888@users.noreply.github.com>
2024-07-25 02:26:11 +0900

    test: 서버 테이블 생성 되는거확인

--- Tetrhythm_sourcecode/SQLTest.cpp
+++ Tetrhythm_sourcecode/SQLTest.cpp
@@ -1,36 +1,36 @@
-//#include <mysql.h>
-//#include <iostream>
-//#pragma comment(lib, "libmySQL.lib")
-//using namespace std;
-//int main() {
-//
-//	MYSQL Conn;
-//	MYSQL* ConnPtr = NULL;
-//	MYSQL_RES* Result;
-//	MYSQL_ROW Row;
-//	int Stat;
-//
-//	mysql_init(&Conn);
-//
-//	ConnPtr = mysql_real_connect(&Conn, "iu51mf0q32fkhfpl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "c4soupya4kvutpg2", "nyryixy9qhnhzfze", "ljk5c9ot0z8wofa5", 3306, (char*)NULL, 0);
-//	if (ConnPtr == NULL) {
-//		cout << mysql_error(&Conn) << endl;
-//		return 1;
-//	}
-//
-//	const char* Query = "SELECT 'star'";
-//	Stat = mysql_query(ConnPtr, Query);
-//	if (Stat != 0) {
-//		cout << mysql_error(&Conn) << endl;
-//		return 1;
-//	}
-//
-//	Result = mysql_store_result(ConnPtr);
-//	while ((Row = mysql_fetch_row(Result)) != NULL) {
-//		cout << Row[0] << endl;
-//	}
-//	mysql_free_result(Result);
-//	mysql_close(ConnPtr);
-//	return 0;
-//
-//}
+#include <mysql.h>
+#include <iostream>
+#pragma comment(lib, "libmySQL.lib")
+using namespace std;
+int main() {
+
+	MYSQL Conn;
+	MYSQL* ConnPtr = NULL;
+	MYSQL_RES* Result;
+	MYSQL_ROW Row;
+	int Stat;
+
+	mysql_init(&Conn);
+
+	ConnPtr = mysql_real_connect(&Conn, "iu51mf0q32fkhfpl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "c4soupya4kvutpg2", "nyryixy9qhnhzfze", "ljk5c9ot0z8wofa5", 3306, (char*)NULL, 0);
+	if (ConnPtr == NULL) {
+		cout << mysql_error(&Conn) << endl;
+		return 1;
+	}
+
+	const char* Query = "SELECT * from test_table";
+	Stat = mysql_query(ConnPtr, Query);
+	if (Stat != 0) {
+		cout << mysql_error(&Conn) << endl;
+		return 1;
+	}
+
+	Result = mysql_store_result(ConnPtr);
+	while ((Row = mysql_fetch_row(Result)) != NULL) {
+		cout << Row[0] << endl;
+	}
+	mysql_free_result(Result);
+	mysql_close(ConnPtr);
+	return 0;
+
+}
--- Tetrhythm_sourcecode/폰트.cpp
+++ Tetrhythm_sourcecode/폰트.cpp
@@ -1,54 +1,54 @@
-#include "Print.h"
-#include "WindowManager.h"
-#include <vector>
-#include <thread>
-#include <chrono>
-#include <iostream>
-
-int main(int argc, char* argv[]) {
-    WindowManager windowManager("Tetrhythm", 1200, 675);
-    Print* print = new Print(&windowManager);
-
-    // 텍스트 출력 테스트
-    TTF_Font* font = print->loadFont("Galmuri9.ttf", 28); // 폰트출력추가
-    if (!font) return -1;
-
-    SDL_Color color = { 255, 255, 255 }; // 흰색
-    print->printText("Input ID", 200, 100, 1, font, color); // 폰트출력추가
-
-    // PNG 출력 테스트
-    print->printPNG("pngImage.png", 300, 300, 2);
-    print->printPNG("testPic.png", 50, 50, 3);
-
-    // 애니메이션 출력 테스트
-    std::vector<std::string> animPaths1 = { "frame1.png", "frame2.png" };
-    print->printAnimationPNG(animPaths1, 200, 200, 4, 60);
-
-    const int targetFPS = 60;
-    const std::chrono::milliseconds frameDuration(1000 / targetFPS);
-    int i = 0;
-    //텍스트 input
-    
-    TTF_Font* font2 = print->loadFont("Galmuri9.ttf", 28); // 폰트출력추가
-    print->InputText(200, 200, 1, font2, color);
-
-    SDL_StartTextInput();
-
-    while (true) {
-        i++;
-        //폰트 입력 동시에 했을 때는 이렇게.
-        print->updateAnimations();
-        print->handleTextEvents();
-        print->renderWithTextInputAndPNG();
-
-        if (i > 60) {
-            print->setText("BBuuBBuu");
-        }
-
-        std::this_thread::sleep_for(frameDuration);
-    }
-    SDL_StopTextInput();
-    print->unloadFont(font); // 폰트출력추가
-    delete print;
-    return 0;
-}
+//#include "Print.h"
+//#include "WindowManager.h"
+//#include <vector>
+//#include <thread>
+//#include <chrono>
+//#include <iostream>
+//
+//int main(int argc, char* argv[]) {
+//    WindowManager windowManager("Tetrhythm", 1200, 675);
+//    Print* print = new Print(&windowManager);
+//
+//    // 텍스트 출력 테스트
+//    TTF_Font* font = print->loadFont("Galmuri9.ttf", 28); // 폰트출력추가
+//    if (!font) return -1;
+//
+//    SDL_Color color = { 255, 255, 255 }; // 흰색
+//    print->printText("Input ID", 200, 100, 1, font, color); // 폰트출력추가
+//
+//    // PNG 출력 테스트
+//    print->printPNG("pngImage.png", 300, 300, 2);
+//    print->printPNG("testPic.png", 50, 50, 3);
+//
+//    // 애니메이션 출력 테스트
+//    std::vector<std::string> animPaths1 = { "frame1.png", "frame2.png" };
+//    print->printAnimationPNG(animPaths1, 200, 200, 4, 60);
+//
+//    const int targetFPS = 60;
+//    const std::chrono::milliseconds frameDuration(1000 / targetFPS);
+//    int i = 0;
+//    //텍스트 input
+//    
+//    TTF_Font* font2 = print->loadFont("Galmuri9.ttf", 28); // 폰트출력추가
+//    print->InputText(200, 200, 1, font2, color);
+//
+//    SDL_StartTextInput();
+//
+//    while (true) {
+//        i++;
+//        //폰트 입력 동시에 했을 때는 이렇게.
+//        print->updateAnimations();
+//        print->handleTextEvents();
+//        print->renderWithTextInputAndPNG();
+//
+//        if (i > 60) {
+//            print->setText("BBuuBBuu");
+//        }
+//
+//        std::this_thread::sleep_for(frameDuration);
+//    }
+//    SDL_StopTextInput();
+//    print->unloadFont(font); // 폰트출력추가
+//    delete print;
+//    return 0;
+//}

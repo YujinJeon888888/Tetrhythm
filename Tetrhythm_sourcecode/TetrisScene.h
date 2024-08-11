e2df8b7c42f54b1cb5a7b02d15301f54f5439777
Siun <164330547+siunp1203@users.noreply.github.com>
2024-08-11 13:49:40 +0900

    feat: 하트노드 움직임

--- Tetrhythm_sourcecode/TetrisScene.h
+++ Tetrhythm_sourcecode/TetrisScene.h
@@ -6,6 +6,7 @@
 #include "MainMenu.h"
 #include "MySQL.h"
 #include "UserInfo.h"
+
 class TetrisScene : public Scene {
 public:
 	TetrisScene(WindowManager& wm, SceneManager& manager); // 클래스 이름에 맞게 생성자 이름 변경 해줘야함.
@@ -14,9 +15,16 @@ public:
 	void update() override;
 	void render() override;
 private:
+	void resetHeart();
+
 	Print* print;
 	WindowManager& windowManager;
 	SceneManager& sceneManager;
 	Game* game;
 
+	int heartPosX; // 하트의 현재 X 좌표
+	int heartSpeed; // 하트의 이동 속도
+	bool heartVisible; // 하트가 보이는지 여부
+
+	double timeSinceStart; // 게임 시작 후 경과 시간
 };

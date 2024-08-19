5073bca4eda37831cdad671fccc647ee513cc828
YujinJeon888888 <117812553+YujinJeon888888@users.noreply.github.com>
2024-08-19 23:47:50 +0900

    feat: 반짝반짝

--- Tetrhythm_sourcecode/scoreTableScene.cpp
+++ Tetrhythm_sourcecode/scoreTableScene.cpp
@@ -1,5 +1,18 @@
 #include "scoreTableScene.h"
-
+MySQL mysql;
+std::string result;
+std::vector<std::string> tokens;
+TTF_Font* font;
+SDL_Color color = { 255, 255, 255 }; // 흰색
+std::vector<SDL_Color> colors = {
+	{255, 0, 0},      // 빨강
+	{255, 165, 0},    // 주황
+	{255, 255, 0},    // 노랑
+	{0, 255, 0},      // 초록
+	{0, 0, 255},      // 파랑
+	{75, 0, 130},     // 남색 (인디고)
+	{238, 130, 238}   // 보라 (바이올렛)
+};
 scoreTableScene::scoreTableScene(WindowManager& wm, SceneManager& manager) : windowManager(wm), sceneManager(manager), print(new Print(&wm))
 {
 	drawInit();
@@ -10,13 +23,10 @@ void scoreTableScene::drawInit()
 	//프린트 초기화
 	print->clearScreen();
 	print->deleteLayer(0);
-	MySQL mysql;
-	std::string result = mysql.printTable();
-	std::vector<std::string> tokens;
+	print->deleteLayer(1);
+	result = mysql.printTable();
 	tokens = splitString(result, '\n');
-	TTF_Font* font = print->loadFont("DungGeunMo.ttf", 18);
-	SDL_Color color = { 255, 255, 255 }; // 흰색
-	SDL_Color yellowColor = { 244, 233, 0 }; // 노랑
+	font = print->loadFont("DungGeunMo.ttf", 18);
 
 	int y = 0;
 	int x = 0;
@@ -25,13 +35,14 @@ void scoreTableScene::drawInit()
 			y = 0;
 			x += 200;
 		}
-		if (y == 0 && x==0) {
-			print->printText(token, x, y, 0, font, yellowColor);
+		if (y == 0 && x == 0) {
+			// 애니메이션 초기화
+			print->textAnimation(token, x, y, font, colors, 8, 1);
 		}
 		else {
 			print->printText(token, x, y, 0, font, color);
 		}
-		
+
 		y += 25;
 	}
 	print->render();
@@ -39,11 +50,12 @@ void scoreTableScene::drawInit()
 
 void scoreTableScene::handleEvents()
 {
-
+	print->handleEvents();
 }
 
 void scoreTableScene::update()
 {
+	print->updateTextAnimation();
 	print->handleEvents([&](SDL_Event& event) {
 		if (event.type == SDL_KEYDOWN) {
 			handleArrowKey(event.key.keysym.sym);
@@ -62,6 +74,7 @@ void scoreTableScene::handleArrowKey(SDL_Keycode key) {
 
 void scoreTableScene::render()
 {
+	print->render();
 }
 
 //std::vector<std::string> splitString(const std::string& str, char delimiter) {
